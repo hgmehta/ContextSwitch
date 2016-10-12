2e9e46e5d6aace4789e973500f4de959a8579d86
hgmehta <harshmehta87.1997.hm@gmail.com>
2016-10-12 20:39:25 +0530

    Two thread and its basic details

---
+++ ContextSwitch/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=contextwsitch
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/contextwsitch
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=contextwsitch.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/contextwsitch.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=contextwsitch
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/contextwsitch
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=contextwsitch.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/contextwsitch.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
