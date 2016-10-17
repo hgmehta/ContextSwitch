f486c50bb0090cdd634553acf44fc0d057254903
hgmehta <harshmehta87.1997.hm@gmail.com>
2016-10-17 11:36:10 +0530

    Basic thread scheduling based on time slice

--- ContextSwitch/scheduling.cpp
+++ ContextSwitch/scheduling.cpp
@@ -12,72 +12,73 @@
 #include <signal.h>
 
 
-void userDefinedSignal(int signal){ // user defined signal to switch the thread execution.  
+void userDefinedSignal(int signal){
 
-	if (SIGUSR1 == signal){
-		pause();        //this will pause the current running thread execution. 
+	if(SIGUSR1 == signal){
+		pause();
+	}
+	if(SIGUSR2 == signal){
+		kill(signal, SIGCONT);
 	}
 }
-void *toDoFunction(void * pv){   //thread will execute this function. 
+void *toDoFunction(void * pv){
 
-	intptr_t thread = (intptr_t) pv;
+	pthread_t thread = (pthread_t) pv;
 
-	pause();    //this will block the current running thread and pause for 4 seconds.
-                    //because the sleep time for currently running thread is 4 seconds.
-	int i = 0;  //initial value of i for each thread is zero.
+	pause();
+	int i = 0;
 	while(1)
 	{
 		printf("Thread %d: count %d\n", (int) thread, i++);
 		sleep(1);
 	}
-
-//	pthread_exit(NULL);
+	pthread_exit(NULL);
 }
 
 int main(int argc, char ** argv){
 
 	struct sigaction signal_action;
-	memset(&signal_action, 0, sizeof(signal_action));   //initialize the signal_action with zeros. 
-	signal_action.sa_handler = userDefinedSignal;   //on signal call userDefinedSignal function will be called.
+	memset(&signal_action, 0, sizeof(signal_action));
+	signal_action.sa_handler = userDefinedSignal;
 
-	sigaction(SIGUSR1, &signal_action, NULL);   //creating the signal action on signal call 
+	sigaction(SIGUSR1, &signal_action, NULL);
 	sigaction(SIGUSR2, &signal_action, NULL);
 
-	pthread_t threads[2];   //initializing the thread.
+	pthread_t threads[2];
 	threads[0] = 0;
 	threads[1] = 0;
 
-	intptr_t thread = 0;
+	pthread_t thread = 0;
 
-	for(thread = 0; thread < 2; thread++){  //creating the thread.
+	for(thread = 0; thread < 2; thread++){
 
 		int isSuccess = pthread_create(&threads[thread], NULL, toDoFunction,(void *) thread);
-		if(isSuccess){      //checking the error whether the thread is created or not   
+		if(isSuccess){
 
 			perror("Error: Problem while creating a thread");
 			exit(1);
 	    	}
 	}
 
-
-	for(thread = 0;thread<=2; thread++){    //for loop will executes continuously. 
+	sleep(1);
+	for(thread = 0;thread<=2; thread++){
 
 		if(thread == 2){
 
-		thread = 0; 
+		thread = 0;
 		}		
 
-		int isSuccess = pthread_kill(threads[thread], SIGUSR2); // resume the current thread.
-		if (isSuccess){     //checking whether the function executes successfully or not 
+		int isSuccess = pthread_kill(threads[thread], SIGUSR2);
+		if (isSuccess){
 
 			perror("Error in resuming thread");
 			exit(1);
 		}
 
-		sleep(5);
+		sleep(4);
 
-		isSuccess = pthread_kill(threads[thread], SIGUSR1); //block the current thread
-		if(isSuccess){      //checking whether the function executes successfully or not 
+		isSuccess = pthread_kill(threads[thread], SIGUSR1);
+		if(isSuccess){
 
 			perror("Error in blocking thread");
 			exit(1);
